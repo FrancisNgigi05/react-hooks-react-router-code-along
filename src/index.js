6c26f4d8519ebd70c4d8944adce855a466d2db3c
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-02-10 20:46:48 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import { Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>Home is cool!</h1>
     </div>
   );
 }
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+    </div>
+  )
+}
 
 function App() {
-  <Route path="/">
-    <Home />
-  </Route>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={ <Login />}></Route>
+    </Routes>
+  )
 }
 
 ReactDOM.render(

6feda69a4b3d8644bd306e806658025dec09bb7f
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-02-10 19:55:33 +0300

    on progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import { Route } from "react-router-dom";
 function Home() {
   return (
     <div>
